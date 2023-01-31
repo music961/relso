9c61b74df83be75bbc3d2214d34bed253bb34914
music961 <961boss@gmail.com>
2023-01-31 23:09:48 +0900

    약간 고침

---
+++ components/Layouts.tsx
@@ -0,0 +1,15 @@
+const layouts = ({children}:any) => {
+    return (
+      <body class="bg-black text-gray-200">
+        <nav class="px-4 py-3 flex justify-center bg-green-800">
+            <a class="mx-4 hover:underline" href="/">릴레이 선언</a>
+            <a class="mx-4 hover:underline" href="/login/loginList">관리자</a>
+        </nav>
+        <div class="flex justify-center">
+          {children}
+        </div>
+      </body>
+    )
+}
+
+export default layouts
