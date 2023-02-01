8e71e24fa410724e154fced134b24b30d447b97e
music961 <961boss@gmail.com>
2023-02-01 18:19:01 +0900

    sdf

--- const/Const.ts
+++ const/Const.ts
@@ -4,8 +4,7 @@ const EMPTY_NUMBER : number = -10000
 const pintoDBHostName = "ec2-43-200-180-89.ap-northeast-2.compute.amazonaws.com"
 const pintoDBUsrName = "relso"
 const pintoDBPassword = "mct1234"
-const S3_ACCOUNT = ["AKIAV32KBQHJDILEL757","SQ8uhm6UTTGzJzAuEt6HJg7xArxs8woh7oi5Qjp9"]
 const version = "0.0.5"
 
 
-export { EMPTY_NUMBER, version, pintoDBHostName, pintoDBUsrName, pintoDBPassword,S3_ACCOUNT }
+export { EMPTY_NUMBER, version, pintoDBHostName, pintoDBUsrName, pintoDBPassword }
