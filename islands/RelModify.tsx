fca00fea777209051c2c95d5b82029765b442720
music961 <961boss@gmail.com>
2023-03-07 18:03:24 +0900

    sdf

--- islands/RelModify.tsx
+++ islands/RelModify.tsx
@@ -28,8 +28,8 @@ export default function FellModify(props : PropsRel){
           </tr>
           <tr>
               <td class="px-4">시작시간</td>
-              <td class="px-4"><input class="bg-black fill-current" id="fellEmail" type="date"/></td>
-              <td class="px-4"><input class="bg-black text-red-500 fill-current" id="fellEmail" type="time"/></td>
+              <td class="px-4"><input class="bg-black hover:fill-current" id="fellEmail" type="date"/></td>
+              <td class="px-4"><input class="bg-black hover:text-red-500 hover:fill-current" id="fellEmail" type="time"/></td>
           </tr>
           <tr>
               <td class="px-4">종료시간</td>
