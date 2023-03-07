c82578bbd0632470f07b2744a812b71cde535c50
music961 <961boss@gmail.com>
2023-03-07 15:34:02 +0900

    df

--- routes/admin/main.tsx
+++ routes/admin/main.tsx
@@ -1,9 +1,7 @@
 import { PageProps, Handlers } from "$fresh/server.ts"
-import { Button, Input } from "../../components/Button.tsx"
 import Layout from "../../components/Layouts.tsx"
 import Admin from "../../islands/Admin.tsx"
 import { select, cnt, isHave } from "../../const/DBTable.tsx"
-import { GoogleOAuth } from "https://deno.land/x/google@0.0.7/oauth.ts"
 import { WithSession } from "freshSession"
 
 export default function main({data}:PageProps){
@@ -23,7 +21,7 @@ export const handler:  Handlers<any,WithSession> = {
       let isLogin = false
       const email = session.data.aabbcc
       if(email!=null){
-          isLogin = await isHave("lim_admin where email=?",[email])
+          isLogin = await isHave("rel_admin where email=?",[email])
       }
       return await cxt.render({
         isLogin : isLogin,
