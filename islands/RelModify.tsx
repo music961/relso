bf61981366b91e05ee7886f4b8bc3c4f4a36ea78
music961 <961boss@gmail.com>
2023-03-08 09:59:18 +0900

    sdf

--- islands/RelModify.tsx
+++ islands/RelModify.tsx
@@ -7,7 +7,7 @@ interface PropsRel {
 
 export default function FellModify(props : PropsRel){
   const rel = props.rel
-  const d = new Date(rel.main_end).toISOString
+  const d = new Date(rel.main_end).toISOString()
     return (
       <div>
       <div>
@@ -99,9 +99,9 @@ const fellSummit = (fellKey:any,url:string)=>{
 }
 
 const fellDel = (fellKey:number)=>{
-  alert('asdfasdf')
+  alert('asdfas')
   let elem = document.getElementById('endTime')
-  elem.value = new Date(13456).toISOString
+  elem.value = new Date(13456).toISOString()
   // if(confirm(`삭제하시겠습니까?`)){
   //     await fetch(
   //         `../DB/fell/runFellDel`,
