85bf82096bf4dc82d9d51b1370792d65ab491914
music961 <961boss@gmail.com>
2023-03-08 09:17:34 +0900

    dsf

--- routes/admin/relModify/[mainKey].tsx
+++ routes/admin/relModify/[mainKey].tsx
@@ -1,12 +1,19 @@
-import { PageProps } from '$fresh/server.ts'
+import { PageProps, Handlers } from '$fresh/server.ts'
+import { select } from "../../../const/DBTable.tsx"
 import Layout from '../../../components/Layouts.tsx'
 import RelModify from '../../../islands/RelModify.tsx'
 
-export default function info_create(props:PageProps){
-    const {mainKey} = props.params
+export default function info_modify({data}:PageProps){
     return (
         <Layout>
-            <RelModify rel={{main_key:mainKey}} url='../../../DB/rel/runRelModify' />
+            <RelModify rel={data} url='../../../DB/rel/runRelModify' />
         </Layout>
     )
+}
+  
+export const handler: Handlers = {
+    async GET(req,cxt){
+        const [comp] = await select("* from rel_main where main_key=?",[cxt.params.mainKey])
+        return await cxt.render(comp)
+    }
 }
