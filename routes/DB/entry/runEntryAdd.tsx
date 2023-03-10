2b0aaa0665aacf303ebdccf15cb76abdf602f161
music961 <961boss@gmail.com>
2023-03-10 16:44:18 +0900

    ㄴㅇㄹ

--- routes/DB/entry/runEntryAdd.tsx
+++ routes/DB/entry/runEntryAdd.tsx
@@ -5,9 +5,10 @@ export const handler = {
     async POST(req:Request):Promise<Response>{
         const entry = await req.json()
         pintoLog(entry)
+        // insert 하기 전, 누군가가 먼저 등록을 했는지 체크하고 진행.
         insert(
           //'rel_entry (main_key,entry_name,entry_start)value (?,?,?)',
-          'rel_entry (main_key,entry_name,entry_start) select ?,?,? where exists(select * from rel_entry where main_key=? and entry_start>? and state is null)'
+          'rel_entry (main_key,entry_name,entry_start) select ?,?,? where exists(select * from rel_entry where main_key=? and entry_start>? and state is null)',
           //[entry.mainKey,entry.entryName,Date.now()]
           [entry.mainKey,entry.entryName,Date.now(),entry.mainKey,Date.now()]
         )
