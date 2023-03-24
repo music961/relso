b19f139fbfae2ed501c59dfc837dc8c1caf52ed1
music961 <961boss@gmail.com>
2023-03-24 15:08:05 +0900

    ㅁㄴㄹㅇ

--- routes/index.tsx
+++ routes/index.tsx
@@ -17,6 +17,8 @@ export default function Home({data}:PageProps) {
       </Head>
       <Layout>
         <div>
+          {"asdfasdf"}
+          {Deno.env.get('민호765')}
           <Relso relso={data.relso}/>
           <Entry th={entrys.length} mainKey={data.mainKey} 이전주자_닉네임={firstEntry.entry_name} entry={data.entry} reserve={data.reserve}/>
           <Entrys entrys={data.entrys}/>
