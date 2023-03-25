7356ca57ceb247ae7deedea5ebf0a9fa39036d91
music961 <961boss@gmail.com>
2023-03-25 17:55:32 +0900

    jlkj

--- const/Const.ts
+++ const/Const.ts
@@ -4,10 +4,6 @@ const oauthId = "602990314098-bvs1v7oj71pmbb9m4ptp9aq68jeg10k7.apps.googleuserco
 const oauthSecret = "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"
 const oauthRedirect = "https://relso.deno.dev/DB/runGoogleAuth"
 
-//릴소사 데이터베이스
-// const pintoDBHostName = "ec2-43-200-180-89.ap-northeast-2.compute.amazonaws.com"
-// const pintoDBUsrName = "relso"
-// const pintoDBPassword = "mct1234"
 const version = "0.0.5"
 
 
