ea354c147ad4e837de097b6e39dccb3fd072bdb2
music961 <961boss@gmail.com>
2023-03-25 20:09:01 +0900

    sfd

--- routes/entry/entryWrite.tsx
+++ routes/entry/entryWrite.tsx
@@ -1,6 +1,7 @@
 import Layout from '../../components/Layouts.tsx'
 import EntryModify from '../../islands/EntryModify.tsx'
 import { PageProps, Handlers } from "$fresh/server.ts"
+import { pintoLog } from '../../const/Function.ts'
 
 export default function entry_create({data}:PageProps){
     const  entry = data.entry || {}
@@ -14,7 +15,10 @@ export default function entry_create({data}:PageProps){
 export const handler: Handlers = {
     async GET(req,cxt){
         const params = new URLSearchParams(req.url.split('?')[1])
-        const decodedEntry = JSON.parse(decodeURIComponent(params.get('entry')!))
+        pintoLog(`url 분리 : ${params}`)
+        const decoded = decodeURIComponent(params)!
+        pintoLog(`디코딩 : ${decoded}`)
+        const decodedEntry = JSON.parse(decoded)
         return await cxt.render({
             entry : decodedEntry
         })
