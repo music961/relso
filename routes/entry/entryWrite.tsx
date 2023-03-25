5c50ef539370898b095eb3ee999f23f09a34de06
music961 <961boss@gmail.com>
2023-03-25 14:23:20 +0900

    sdf

--- routes/entry/entryWrite.tsx
+++ routes/entry/entryWrite.tsx
@@ -1,10 +1,24 @@
 import Layout from '../../components/Layouts.tsx'
 import EntryModify from '../../islands/EntryModify.tsx'
+import { PageProps, Handlers } from "$fresh/server.ts"
 
-export default function entry_create(){
+export default function entry_create({data}:PageProps){
     return (
         <Layout>
+            {data.name}
+            {data.age}
             <EntryModify entry={{}} url='../../DB/entry/runEntryWrited' />
         </Layout>
     )
+}
+
+export const handler: Handlers = {
+    async POST(req,cxt){
+        const body = await req.json()
+
+        return await cxt.render({
+            name : body.name,
+            age : body.age
+        })
+    }
 }
