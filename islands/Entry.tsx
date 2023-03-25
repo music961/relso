584aac33735e9e888c10833d32cfd413e85d6db7
music961 <961boss@gmail.com>
2023-03-25 20:03:03 +0900

    sdf

--- islands/Entry.tsx
+++ islands/Entry.tsx
@@ -49,7 +49,9 @@ export default function Entry(props:EntryProps){
                     <tr>
                         <td class="px-4">
                         <Link href="/entry/entryWrite" data={encodeURIComponent(JSON.stringify({name:"대민호",age:18}))}>
-                            <Button>제출22</Button>
+                            <Button onClick={()=>{
+                                route(`/entry/entryWrite?name=${"대민호"}&age=${18}`)
+                            }}>제출22</Button>
                         </Link>
                         <Button onClick={()=>entrySummit(entry.entry_key,1,"제출하시겠습니까?")}>제출</Button>
                         <Button onClick={()=>entrySummit(entry.entry_key,2,"포기하시겠습니까?")}>포기</Button>
