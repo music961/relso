79c8a386e9db18da5eb6ed068ca477b4f033dc3d
music961 <961boss@gmail.com>
2023-03-25 18:30:07 +0900

    sdf

--- routes/entry/entryWrite.tsx
+++ routes/entry/entryWrite.tsx
@@ -2,6 +2,7 @@ import Layout from '../../components/Layouts.tsx'
 import EntryModify from '../../islands/EntryModify.tsx'
 import { PageProps, Handlers } from "$fresh/server.ts"
 import { pintoLog } from '../../const/Function.ts'
+import { WithSession } from 'https://deno.land/x/fresh_session@0.2.0/mod.ts'
 
 export default function entry_create({data}:PageProps){
     const  entry = data.entry || {}
@@ -12,14 +13,14 @@ export default function entry_create({data}:PageProps){
     )
 }
 
-// export const handler: Handlers = {
-//     async POST(req,cxt){
-//         const body = await req.json()
-//         const name = body.name || '민'
-//         const age = body.age || 10
-//         pintoLog(`받았음 ${body.name}`)
-//         return cxt.render({
-//             entry : name
-//         })
-//     }
-// }
+export const handler: Handlers<any,WithSession> = {
+    async POST(req,cxt){
+        const body = await req.json()
+        const name = body.name || '민'
+        const age = body.age || 10
+        pintoLog(`받았음 ${body.name}`)
+        return cxt.render({
+            entry : name
+        })
+    }
+}
