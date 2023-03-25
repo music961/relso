a17e4f9d79eb34850511601441415b726f41a553
music961 <961boss@gmail.com>
2023-03-25 18:45:05 +0900

    ㄴㅇㄹ

--- routes/entry/entryWrite.tsx
+++ routes/entry/entryWrite.tsx
@@ -8,7 +8,7 @@ export default function entry_create({data}:PageProps){
     const  entry = data.entry || {}
     return (
         <Layout>
-            <EntryModify entry={'민훠어'} url='../../DB/entry/runEntryWrited' />
+            <EntryModify entry={entry} url='../../DB/entry/runEntryWrited' />
         </Layout>
     )
 }
@@ -32,7 +32,23 @@ export const handler: Handlers = {
         // const age = body.age || 10
         // pintoLog(`받았음 ${body.name}`)
         return await cxt.render({
-            entry : '민민'
+            entry : {name:'민민',age:10}
         })
     }
 }
+
+export const handler2: Handlers = {
+    async POST(req,cxt){
+        // let n
+        // const body = await req.json()
+        // if(body){
+        //     n = body.name
+        // }
+        // const age = body.age || 10
+        // pintoLog(`받았음 ${body.name}`)
+        return await cxt.render({
+            entry : {name:'민민후후',age:12}
+        })
+    }
+}
+
