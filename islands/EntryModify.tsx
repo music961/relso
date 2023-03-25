1597c9d20211d7123d1b7c9099888f13a464f416
music961 <961boss@gmail.com>
2023-03-25 18:46:05 +0900

    ㄴㅇㄹ

--- islands/EntryModify.tsx
+++ islands/EntryModify.tsx
@@ -8,20 +8,19 @@ interface PropEntity {
 }
 
 export default function EntryModify(props : PropEntity){
-  // const [entry, setEntry] = useState(props.entry);
-  // pintoLog(`트레이스1 ${JSON.stringify(entry)}`)
-  // useEffect(() => {
-  //   pintoLog(`트레이스2 ${JSON.stringify(entry)}`)
-  //   setEntry(props.entry);
-  // }, [entry]);
+  const [entry, setEntry] = useState(props.entry);
+  pintoLog(`트레이스1 ${JSON.stringify(entry)}`)
+  useEffect(() => {
+    pintoLog(`트레이스2 ${JSON.stringify(entry)}`)
+    setEntry(props.entry);
+  }, [entry]);
 
-  // if (!entry) {
-  //   return <div>Loading...</div>;
-  // }else{
+  if (!entry) {
+    return <div>Loading...</div>;
+  }else{
     return (
       <div>
-        ㅁㄴㅇㅁㄴㄻㄴㅇㄻㄴㄹ
-        {/* <table class="p-4 shadow-md">
+        <table class="p-4 shadow-md">
             <tr>
                 <td class="px-4">이름</td>
                 <td><Input id="relRound" value={entry.name}/></td>
@@ -30,7 +29,7 @@ export default function EntryModify(props : PropEntity){
                 <td class="px-4">제목</td>
                 <td><Input id="relTitle" value={entry.age}/></td>
             </tr>
-        </table> */}
+        </table>
       </div>
     )
   //}
