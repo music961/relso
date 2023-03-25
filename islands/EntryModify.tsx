fc878d2f972d579be5b735f1f9074f5a51732717
music961 <961boss@gmail.com>
2023-03-25 15:09:54 +0900

    dsf

--- islands/EntryModify.tsx
+++ islands/EntryModify.tsx
@@ -8,11 +8,18 @@ interface PropsRel {
 }
 
 export default function EntryModify(props : PropsRel){
-  const [entry, setEntry] = useState(props.entry); // useState 훅을 이용해 entry 상태를 관리합니다.
+  const [entry, setEntry] = useState(props.entry)
+  
+  const handleChange = (event: any) => {
+    const { id, value } = event.target
+    setEntry((prevEntry) => ({
+      ...prevEntry,
+      [id]: value
+    }))
+  }
   
   pintoLog(`모디파이에서 받은거 ${JSON.stringify(entry)}`)
   let result = (<div/>)
-  
   if(entry){
     result = (
       <div>
@@ -20,18 +27,15 @@ export default function EntryModify(props : PropsRel){
         <table class="p-4 shadow-md">
             <tr>
                 <td class="px-4">라운드</td>
-                <td><Input id="relRound" value={entry.round} onChange={(e) => setEntry({...entry, round: e.target.value})}/></td>
-                {/* onChange 핸들러를 이용해 Input 컴포넌트의 값을 업데이트합니다. */}
+                <td><Input id="relRound" value={entry.relRound} onChange={handleChange}/></td>
             </tr>
             <tr>
                 <td class="px-4">제목</td>
-                <td><Input id="relTitle" value={entry.title} onChange={(e) => setEntry({...entry, title: e.target.value})}/></td>
-                {/* onChange 핸들러를 이용해 Input 컴포넌트의 값을 업데이트합니다. */}
+                <td><Input id="relTitle" value={entry.relTitle} onChange={handleChange}/></td>
             </tr>
         </table>
       </div>
     )
   }
-  
-  return result;
+  return result
 }
