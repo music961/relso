6b8599fdfb2ee93dd532a27234d126f0905dc2c7
music961 <961boss@gmail.com>
2023-03-30 15:05:33 +0900

    ㄴㅇㄹ

--- routes/admin/relList.tsx
+++ routes/admin/relList.tsx
@@ -25,7 +25,9 @@ export const handler:  Handlers<any,WithSession> = {
         isLogin = await isHave("rel_admin where email=?",[email])
     }
     const cntAdmin = await cnt("rel_main")
-    const selectAdmin = await select("* from rel_main rm left join (select main_key,entry_name from rel_entry where entry_key in (select min(entry_key) from rel_entry group by main_key))re on rm.main_key = re.main_key")
+    const selectAdmin = await select(
+      "*,CONCAT_WS('-', MONTH(FROM_UNIXTIME(main_start /1000)), ROW_NUMBER() OVER(PARTITION BY YEAR(FROM_UNIXTIME(main_start/1000)), MONTH(FROM_UNIXTIME(main_start/1000)) ORDER BY FROM_UNIXTIME(main_start/1000))) AS round from rel_main rm left join (select main_key,entry_name from rel_entry where entry_key in (select min(entry_key) from rel_entry group by main_key))re on rm.main_key = re.main_key"
+      )
     return await cxt.render({
       isLogin : isLogin,
       cnt : cntAdmin,
