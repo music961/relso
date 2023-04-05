f5f44b400737e8a5c3ea8de81307563fa6565b42
music961 <961boss@gmail.com>
2023-04-05 18:09:14 +0900

    sdf

--- islands/Entrys.tsx
+++ islands/Entrys.tsx
@@ -12,13 +12,13 @@ export default function Entrys(props: RelsProps) {
         const cnt = entrys.length
         return (
             <div>
-                <table class="p-4">
+                <table class="p-4 flex justify-center">
                     <th>번째</th>
                     <th>작가</th>
                     <th>제출시간</th>
                     {
                         entrys.map((entry,idx)=>
-                            <tr class="mx-4 flex justify-center">
+                            <tr class="mx-4">
                                 <td class="px-4">{cnt-idx}</td>
                                 <td class="px-4">{entry.entry_name}</td>
                                 <td class="px-4">{convertDate(entry.entry_end).substring(4)}</td>
