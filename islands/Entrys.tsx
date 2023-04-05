8981f85674cbbb4e20124c0c491fd5e196031121
music961 <961boss@gmail.com>
2023-04-05 16:55:28 +0900

    ㄴㅇㄹㄴㅇㄹ

--- islands/Entrys.tsx
+++ islands/Entrys.tsx
@@ -7,6 +7,7 @@ interface RelsProps {
 
 export default function Entrys(props: RelsProps) {
     const entrys = props.entrys
+    const 역순 = entrys.reverse()
     if(entrys){
         const cnt = entrys.length
         return (
@@ -16,9 +17,9 @@ export default function Entrys(props: RelsProps) {
                     <th>작가</th>
                     <th>제출시간</th>
                     {
-                    entrys.reverse().map((entry,idx)=>
+                    역순.map((entry,idx)=>
                         <tr class="mx-4">
-                            <td class="px-4">{cnt-idx}번</td>
+                            <td class="px-4">{cnt-idx}</td>
                             <td class="px-4">{entry.entry_name}</td>
                             <td class="px-4">{convertDate(entry.entry_end).substring(4)}</td>
                         </tr>
