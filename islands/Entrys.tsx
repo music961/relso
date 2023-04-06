5b613afdb23803b3a4a525f0bac52b0d3dad56e8
music961 <961boss@gmail.com>
2023-04-06 12:49:35 +0900

    sdf

--- islands/Entrys.tsx
+++ islands/Entrys.tsx
@@ -28,7 +28,7 @@ export default function Entrys(props: RelsProps) {
                                 {idx+1}번째 [{entry.entry_name}]님
                             </div>
                             <br/>
-                            <pre>
+                            <pre class='block w-full overflow-x-auto max-w-md mx-auto whitespace-pre-wrap'>
                                 {props.novels[idx]}
                             </pre>
                             <br/>
