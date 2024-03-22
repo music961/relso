20ac1ca934e5e0c068cf53c253ee71efb4186c54
music961 <961boss@gmail.com>
2024-03-22 09:17:08 +0900

    테스트 : cxtEntry 제거

--- const/Const.ts
+++ const/Const.ts
@@ -5,10 +5,10 @@ const oauthSecret = "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"
 const oauthRedirect = "https://relso.deno.dev/DB/runGoogleAuth"
 
 // 240322 앤디 : Entrys를 초기화시키는 용도로 사용
-const cxtEntry = useSignal({
-    round : '',
-    topic : '',
-    novel : ''
-})
+// const cxtEntry = useSignal({
+//     round : '',
+//     topic : '',
+//     novel : ''
+// })
 
-export { oauthId, oauthRedirect, oauthSecret, cxtEntry}
+export { oauthId, oauthRedirect, oauthSecret}
