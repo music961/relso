d045e1dad6b02a35b6c59cfbf7b26c90263ac41f
music961 <961boss@gmail.com>
2024-03-22 19:17:43 +0900

    asdfasdfadsf

--- routes/entry/entryWrite.tsx
+++ routes/entry/entryWrite.tsx
@@ -1,6 +1,7 @@
 import Layout from '../../components/Layouts.tsx'
 import EntryModify from '../../islands/EntryModify.tsx'
 import { PageProps, Handlers } from "$fresh/server.ts"
+import { pintoLog } from "../../const/Function.ts";
 
 export default function entry_create({data}:PageProps){
     const  relso = data.relso || {}
@@ -17,6 +18,7 @@ export const handler: Handlers = {
         const params = new URLSearchParams(req.url.split('?')[1])
         const relso = JSON.parse(decodeURIComponent(params.get('relso')!))
         const entry = JSON.parse(decodeURIComponent(params.get('entry')!))
+        pintoLog(relso)
         return await cxt.render({
             relso : relso,
             entry : entry,
